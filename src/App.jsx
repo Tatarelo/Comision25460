61add77b5c27aea9cac6648e5cb50bc1f62cd1c0
Facundo Filippini <facundofilippini@outlook.com>
2022-02-21 23:27:22 -0300

    Desafio 4 componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
-import TituloApp from "./components/Titulo/TituloApp";
-import InputApp from "./components/input/input";
-import ComponenteContenedor from "./components/ComponenteContenedor";
-import ComponenteImagen from "./components/Titulo/IconoCart";
+// import TituloApp from "./components/Titulo/TituloApp";
+// import InputApp from "./components/input/input";
+// import ComponenteContenedor from "./components/ComponenteContenedor";
+// import ComponenteImagen from "./components/Titulo/IconoCart";
 import NavBar from "./components/NavBar/NavBar";
+import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
 
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
   return (
     <div className="App">
         <NavBar/>
+        <ItemListContainer saludo = "Soy una propiedad"/>
         {/* <TituloApp titulo={tit} subtitulo='soy subtitulo' saludo={saludo}/> */}
         {/* <InputApp><ComponenteImagen/><ComponenteImagen/></InputApp> */}
         {/* <ComponenteContenedor saludo='hola soy saludo de contenedor'/> */}
---
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function ItemListContainer({saludo}) {
+  return (
+    <div>{saludo}</div>
+  )
+}
