efb83a555662487f7630114a5bd236ea044716cc
Facundo Filippini <facundofilippini@outlook.com>
2022-03-14 19:50:35 -0300

    Desafio Sincronizar Counter

--- src/components/ItemCart/ItemCart.jsx
+++ src/components/ItemCart/ItemCart.jsx
@@ -2,7 +2,7 @@ import { NavLink } from "react-router-dom";
 
 export const ItemCart = () => {
   return (
-    <NavLink to="Cart">
+    <NavLink to="/cart">
       <img
         className="logoCarrito"
         src="https://cdn-icons-png.flaticon.com/512/1170/1170678.png"
--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -1,5 +1,9 @@
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
 import { ItemCount } from "../ItemCount/ItemCount";
 export const ItemDetail = ({ productos }) => {
+  const [congrat, setCongrat] = useState(false);
+  console.log(congrat);
   return (
     <div className="detailWrapper">
       <p className="detailWrapper_text">{productos.detail}</p>
@@ -11,11 +15,14 @@ export const ItemDetail = ({ productos }) => {
       />
       <p className="detailWrapper_text">{productos.precio}</p>
       <label className="count">Stock disponible:{productos.stock}</label>
-      <ItemCount
-        stock={productos.stock}
-        initial={0}
-        onAdd={(n) => console.log(`Producto${n}`)}
-      />
+      {congrat && <NavLink to='/cart'><button className="terminarCompra" >Terminar compra</button></NavLink>}
+      {!congrat && (
+        <ItemCount
+          stock={productos.stock}
+          initial={0}
+          onAdd={(n) => setCongrat(true)}
+        />
+      )}
     </div>
   );
 };
