e7434c755106c265d324916afdc1e136ca483956
Facundo Filippini <facundofilippini@outlook.com>
2022-03-28 21:22:25 -0300

    Firebase 2

--- src/components/ItemCart/ItemCart.jsx
+++ src/components/ItemCart/ItemCart.jsx
@@ -4,15 +4,19 @@ import { useCartContext } from "../../context/CartContext";
 export const ItemCart = () => {
   const {cantidadTotal} = useCartContext()
   return (
-    <div>
-    <NavLink to="/cart">
+    <>
+    {cantidadTotal > 0 && (
+      <div>
+    <p className="itemsCart">{cantidadTotal}</p>
+    </div>
+    )}
+        <NavLink to="/cart">
       <img
         className="logoCarrito"
         src="https://cdn-icons-png.flaticon.com/512/1170/1170678.png"
         alt="LogoCarrito"
       />
     </NavLink>
-    <p className="itemsCart">{cantidadTotal}</p>
-    </div>
+    </>
   );
 };
